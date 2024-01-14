f64f5181172b24f43f55166d0f5f0c7e738c563e
Dongsheng Wei <weidongsheng@dajiazhongyi.com>
2024-01-14 22:57:05 +0800

    tree/travelTree

--- tree/travelTree.ts
+++ tree/travelTree.ts
@@ -13,40 +13,142 @@ class TreeNode {
 }
 
 /**
- * 中序遍历递归
+ * 中序遍历递归实现
  * @param root 
  */
-function middleTravel(root: TreeNode | null): number[] {
+function inOrderTraversal(root: TreeNode | null): number[] {
   if(root) {
-    return [...middleTravel(root.left), root.val, ...middleTravel(root.right)]
+    return [...inOrderTraversal(root.left), root.val, ...inOrderTraversal(root.right)]
   }else {
     return []
   }
 }
 
 /**
+ * 中序递归迭代实现
+ * @param root 
+ */
+
+function inOrderTraversalIterate(root: TreeNode | null): number[] {
+
+  // if(root === null) return []
+  // const ret = []
+  // const stack: TreeNode[] = []
+  // stack.push(root)
+  // while(stack.length) {
+  //   let node: TreeNode = stack[stack.length - 1]
+  //   while(node.left) {
+  //     stack.push(node.left)
+  //     node = node.left
+  //   }
+  //   node = stack.pop() as TreeNode
+  //   ret.push(node.val)
+  //   if(node.right) {
+  //     stack.push(node.right)
+  //   }else {
+  //     node = stack.pop() as TreeNode
+  //     ret.push(node.val)
+  //   }
+  // }
+  // return ret
+
+  if(root === null) return []
+  const ret = []
+  const stack: TreeNode[] = []
+  let node: TreeNode | null = root
+
+  while(stack.length || node) {
+    while(node) {
+      stack.push(node)
+      node = node.left
+    }
+    node = stack.pop() as TreeNode
+    ret.push(node.val)
+    node = node.right
+  }
+  return ret
+}
+
+/**
  * 前序遍历递归实现
  * @param root 
  */
-function preTravel(root: TreeNode | null) : void {
+function preOrderTraversal(root: TreeNode | null) : number[] {
   if(root) {
-    console.log(root.val)
-    preTravel(root.left)
-    preTravel(root.right)
+    return [root.val, ...preOrderTraversal(root.left), ...preOrderTraversal(root.right)]
+  }else {
+    return []
   }
+}
+
+/**
+ * 前序遍历的非递归过程
+ * @param root 
+ */
+function preOrderTraversalIterate(root: TreeNode | null) : number[] {
+  if(root === null) return []
+  const stack: TreeNode[] = []
+  const ret = []
+  // stack.push(root)
+  let node: TreeNode | null = root
+  while(stack.length || node) {
+    while(node) {
+      ret.push(node.val)
+      stack.push(node)
+      node = node.left
+    }
+    node = stack.pop() as TreeNode
+    node = node.right
+  }
+  return ret
 }
 
 /**
  * 后续遍历递归实现
  * @param root 
  */
-function postTravel(root: TreeNode | null) : void {
+function postOrderTraversal(root: TreeNode | null) : number[] {
   if(root) {
-    postTravel(root.left)
-    postTravel(root.right)
-    console.log(root.val)
+    return [...postOrderTraversal(root.left), ...postOrderTraversal(root.right), root.val]
+  }else {
+    return []
   }
 }
+
+/**
+ * 后序遍历的非递归实现
+ * @param root 
+ */
+function postOrderTraversalIterate(root: TreeNode | null): number[] {
+  if (root === null) return [];
+
+  const ret: number[] = [];
+  const stack: TreeNode[] = [];
+  let prev: TreeNode | null = null;
+  let node: TreeNode | null = root;
+  while (node || stack.length) {
+    while (node) {
+      stack.push(node);
+      root = node.left;
+    }
+
+    node = stack.pop() as TreeNode;
+
+    // 检查右子树是否存在或者是否已经访问过
+    if (!node.right || node.right === prev) {
+      ret.push(node.val);
+      prev = node;
+      node = null; // 置空root，以便下一次弹出栈顶元素
+    } else {
+      // 将当前节点重新入栈，处理右子树
+      stack.push(node);
+      node = node.right;
+    }
+  }
+
+  return ret;
+}
+
 
 (() => {
   const root = new TreeNode(1, 
@@ -57,7 +159,6 @@ function postTravel(root: TreeNode | null) : void {
       new TreeNode(6, null, null), 
       new TreeNode(7, null, null))
   )
-  console.log(middleTravel(root))
-  // preTravel(root)
-  // postTravel(root)
+  console.log(postOrderTraversal(root))
+  console.log(postOrderTraversalIterate(root))
 })()
